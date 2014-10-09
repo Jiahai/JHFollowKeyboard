ffa4fce5a2073ec5b0d29d2e3014c56b007957f0
Jiahai <Jiahai@jiahaideMacBook-Pro.local>
2014-10-09 09:33:15 +0800

    Initial Commit

---
+++ JHFollowKeyboardExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JHFollowKeyboardExample
+//
+//  Created by Jiahai on 14-10-9.
+//  Copyright (c) 2014年 Jiahai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JHFollowKeyboardExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JHFollowKeyboardExample
+//
+//  Created by Jiahai on 14-10-9.
+//  Copyright (c) 2014年 Jiahai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
