7a6703051dc98a3b9142aa32d02e2030830e26b6
Jiahai <xingjiahai@126.com>
2015-09-22 15:08:09 +0800

    添加跟随键盘弹出方式

--- JHFollowKeyboard/UIView+JHFollowKeyboard.h
+++ JHFollowKeyboard/UIView+JHFollowKeyboard.h
@@ -8,15 +8,21 @@
 
 #import <UIKit/UIKit.h>
 
+typedef NS_ENUM(NSInteger,JHFollowKeyboardType) {
+    JHFollowKeyboardType_Auto,              //自动计算，是否被遮挡
+    JHFollowKeyboardType_Moving             //跟随键盘移动
+};
+
 @interface UIView (JHFollowKeyboard)
 
 @property (nonatomic, assign)       CGRect  endKeyboardRect;
 @property (nonatomic, assign)       CGFloat deltaY;                 //View移动的距离
+@property (nonatomic, assign)       JHFollowKeyboardType jhFollowKeyboardType;
 
 /**
  *  开启视图跟随键盘移动效果
  */
-- (void)openFollowKeyboard;
+- (void)openFollowKeyboard:(JHFollowKeyboardType)type;
 /**
  *  关闭视图跟随键盘移动效果
  */
